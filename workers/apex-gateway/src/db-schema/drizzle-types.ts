dab4c715b41b85030b498133a66ceacbcfa3eb65
Adishwar Rishi <adiswa123@gmail.com>
2023-10-13 12:28:34 +1100

    Create db schema for apex-worker

---
+++ workers/apex-gateway/src/db-schema/drizzle-types.ts
@@ -0,0 +1,27 @@
+import { sql } from 'drizzle-orm';
+import { customType } from 'drizzle-orm/sqlite-core';
+import { formatISO, isValid, parseISO } from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
+
+export const dateTime = customType<{ data: Date; driverData: string }>({
+  dataType() {
+    return 'text';
+  },
+  toDriver(value: Date): string {
+    if (isValid(value)) {
+      return formatISO(value);
+    }
+    throw new Error(`DateTime is invalid`);
+  },
+  fromDriver(value: string): Date {
+    // Try parsing as ISO first
+    const result: Date = parseISO(value);
+    if (isValid(result)) {
+      return utcToZonedTime(result, 'UTC'); // Convert to UTC
+    }
+
+    throw new Error(`DateTime is invalid`);
+  },
+});
+
+export const dateTimeDefaultSQL = sql<string>`strftime('%Y-%m-%dT%H:%M:%SZ', 'now', 'utc')`;
---
+++ workers/apex-gateway/src/db-schema/schema.ts
@@ -0,0 +1,95 @@
+import { sqliteTable, integer, text, index, AnySQLiteColumn } from 'drizzle-orm/sqlite-core';
+import { InferSelectModel, InferInsertModel } from 'drizzle-orm';
+import { dateTime, dateTimeDefaultSQL } from './drizzle-types';
+
+export const folderTable = sqliteTable(
+  'folder',
+  {
+    id: integer('id').notNull().primaryKey({ autoIncrement: true }),
+    createdAt: dateTime('created_at').notNull().default(dateTimeDefaultSQL),
+    updatedAt: dateTime('updated_at').notNull().default(dateTimeDefaultSQL),
+    parent_id: integer('parent_id').references((): AnySQLiteColumn => folderTable.id),
+    name: text('name').notNull(),
+    path: text('path').notNull(),
+  },
+  (table) => ({
+    pathIdx: index('idx_folder_path').on(table.path),
+    parentIdIdx: index('idx_folder_parent_id').on(table.parent_id),
+  })
+);
+export type FolderInsertType = InferInsertModel<typeof folderTable>;
+export type FolderSelectType = InferSelectModel<typeof folderTable>;
+
+export const assetTable = sqliteTable(
+  'asset',
+  {
+    id: integer('id').notNull().primaryKey({ autoIncrement: true }),
+    createdAt: dateTime('created_at').notNull().default(dateTimeDefaultSQL),
+    updatedAt: dateTime('updated_at').notNull().default(dateTimeDefaultSQL),
+    deletedAt: dateTime('deleted_at'),
+    folder_id: integer('folder_id')
+      .notNull()
+      .references(() => folderTable.id),
+    guid: text('guid').notNull(),
+    type: text('type', { enum: ['RICH_TEXT', 'VIDEO'] }).notNull(),
+    latest_version_id: integer('latest_version_id')
+      .notNull()
+      .references((): AnySQLiteColumn => assetVersionTable.id),
+  },
+  (table) => ({
+    guidIdx: index('idx_asset_guid').on(table.guid),
+    folderIdIdx: index('idx_asset_folder_id_type_deleted_at').on(
+      table.folder_id,
+      table.type,
+      table.deletedAt
+    ),
+  })
+);
+export type AssetInsertType = InferInsertModel<typeof assetTable>;
+export type AssetSelectType = InferSelectModel<typeof assetTable>;
+
+export const assetVersionTable = sqliteTable(
+  'asset_version',
+  {
+    id: integer('id').notNull().primaryKey({ autoIncrement: true }),
+    createdAt: dateTime('created_at').notNull().default(dateTimeDefaultSQL),
+    updatedAt: dateTime('updated_at').notNull().default(dateTimeDefaultSQL),
+    assetId: integer('asset_id')
+      .notNull()
+      .references(() => assetTable.id),
+    versionSlug: text('version_slug').notNull(),
+    content: text('content', { mode: 'json' }).notNull(),
+  },
+  (table) => ({
+    assetIdVersionIdx: index('idx_asset_version_asset_id_version_slug').on(
+      table.assetId,
+      table.versionSlug
+    ),
+  })
+);
+export type AssetVersionInsertType = InferInsertModel<typeof assetVersionTable>;
+export type AssetVersionSelectType = InferSelectModel<typeof assetVersionTable>;
+
+export const metafieldTable = sqliteTable(
+  'metafield',
+  {
+    id: integer('id').notNull().primaryKey({ autoIncrement: true }),
+    createdAt: dateTime('created_at').notNull().default(dateTimeDefaultSQL),
+    updatedAt: dateTime('updated_at').notNull().default(dateTimeDefaultSQL),
+    assetVersionId: integer('asset_version_id')
+      .notNull()
+      .references(() => assetVersionTable.id),
+    key: text('key').notNull(),
+    value: text('value').notNull(),
+    type: text('type', { enum: ['TEXT', 'DATE'] }).notNull(),
+  },
+  (table) => ({
+    assetVersionIdKeyIdx: index('idx_metafield_asset_version_id_key').on(
+      table.assetVersionId,
+      table.key
+    ),
+    keyValueIdx: index('idx_metafield_key_value').on(table.key, table.value),
+  })
+);
+export type MetafieldInsertType = InferInsertModel<typeof metafieldTable>;
+export type MetafieldSelectType = InferSelectModel<typeof metafieldTable>;
