3e14e7dca4736a62233c91813d632eadf41a7b6e
Adishwar Rishi <adiswa123@gmail.com>
2023-10-15 20:53:49 +1100

    Fix lint in PR 1 merge

--- apps/editor-webapp/app/routes/_index/route.tsx
+++ apps/editor-webapp/app/routes/_index/route.tsx
@@ -1,5 +1,4 @@
 import type { MetaFunction } from '@remix-run/cloudflare';
-import { Undo } from 'lucide-react';
 import DocumentEditor from '~/components/document-editor';
 import { Navbar } from '~/components/navbar';
 
@@ -13,18 +12,18 @@ export default function Index() {
       <main className="flex">
         <div className="fixed w-96 left-0 top-0">
           <div id="current-selection" className=" h-screen border-r-2 bg-gray-100">
-            <div className='p-6'>
-              <div className='text-4xl font-bold leading-normal mb-12'>TIPTOP</div>
+            <div className="p-6">
+              <div className="text-4xl font-bold leading-normal mb-12">TIPTOP</div>
               <div>
-                <div className='py-2 text-lg'>Editor</div>
-                <div className='py-2 text-lg'>Media</div>
+                <div className="py-2 text-lg">Editor</div>
+                <div className="py-2 text-lg">Media</div>
               </div>
             </div>
           </div>
         </div>
         <div className="ml-96 w-full">
           <Navbar />
-          <div className='p-6 mt-16'>
+          <div className="p-6 mt-16">
             <DocumentEditor />
           </div>
         </div>
