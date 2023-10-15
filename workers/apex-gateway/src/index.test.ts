ced5911a03803ddaf825d0c614572124c6f2a2e1
Adishwar Rishi <adiswa123@gmail.com>
2023-10-15 15:22:45 +1100

    Fix unit tests

--- workers/apex-gateway/src/index.test.ts
+++ workers/apex-gateway/src/index.test.ts
@@ -1,7 +1,7 @@
 import { unstable_dev } from 'wrangler';
 import type { UnstableDevWorker } from 'wrangler';
 import { describe, expect, it, beforeAll, afterAll, beforeEach, afterEach } from 'vitest';
-import { graphqlServer } from './index';
+import serverModule from './index';
 import { Env } from '.';
 
 describe('apex-gateway worker', () => {
@@ -40,7 +40,7 @@ describe('apex-gateway worker', () => {
 
   it('should respond to the ping route via invoking the app', async () => {
     const req = new Request('http://localhost/health', { method: 'GET' });
-    const res = await graphqlServer.fetch(req, workerEnv, ctx);
+    const res = await serverModule.fetch(req, workerEnv, ctx);
     expect(res.status).toBe(200);
   });
 });
--- workers/apex-gateway/src/index.ts
+++ workers/apex-gateway/src/index.ts
@@ -12,18 +12,25 @@ export type Env = {
 };
 export type GraphQLContext = YogaInitialContext & Env & ExecutionContext;
 
+// @ts-expect-error - Miniflare needs all exported values to be an object or function
+export let graphqlServer: ReturnType<typeof createYoga<Env & ExecutionContext>> = { INIT_ME: true };
+
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    const graphqlServer = createYoga<Env & ExecutionContext>({
-      schema: createSchema({ typeDefs, resolvers }),
-      plugins: [
-        useResponseCache({
-          cache: createKvCache({ KV: env.GRAPHQL_RESPONSE_CACHE, ctx }),
-          session: () => null,
-          includeExtensionMetadata: true,
-        }),
-      ],
-    });
+    // @ts-expect-error - Miniflare needs all exported values to be an object or function
+    if (graphqlServer.INIT_ME) {
+      console.log('INIT SERVER');
+      graphqlServer = createYoga<Env & ExecutionContext>({
+        schema: createSchema({ typeDefs, resolvers }),
+        plugins: [
+          useResponseCache({
+            cache: createKvCache({ KV: env.GRAPHQL_RESPONSE_CACHE, ctx }),
+            session: () => null,
+            includeExtensionMetadata: true,
+          }),
+        ],
+      });
+    }
     return graphqlServer.fetch(request, env, ctx);
   },
 };
